53396ca93c1d7ce477cb2874c0d66b5972331778
diegompin <diegompin@gmail.com>
2017-03-22 18:43:44 -0400

    gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='data_science',
+    version='0.1',
+    packages=['', 'common', 'plotting', 'statistician', 'network_science', 'machine_learning'],
+    url='',
+    license='',
+    author='diegopinheiro',
+    author_email='diegompin@gmail.com',
+    description=''
+)
