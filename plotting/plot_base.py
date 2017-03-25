e13989d5e16b58358759bbda54c30cbbaa1abaa9
diegompin <diegompin@gmail.com>
2017-03-25 12:30:45 -0400

    curves

--- plotting/plot_base.py
+++ plotting/plot_base.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 class PlotBase(object):
 
 
-    markers = ['D', 's', 'x', '^', 'd', 'h', '+', '*', ',', 'o', '.', '1', 'p', '3', '2', '4', 'H', 'v', '8',
+    markers = ['D', 'o', 'x','s', '^', 'd', 'h', '+', '*', ',', '.', '1', 'p', '3', '2', '4', 'H', 'v', '8',
                '<', '>']
     colors = ['g', 'y', 'r', 'b', 'black']
 
